3bc2991b62e6abd24d0a1b080c13322209641c87
adutta158 <44128207+adutta158@users.noreply.github.com>
2019-05-30 21:33:21 -0700

    Add demo app

---
+++ src/demo_app.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+import pickle
+from keras.models import load_model
+import util
+
+def main():
+    # Load model
+    model = load_model('cnn_b50_e1.h5')
+    image = np.zeros((600, 600, 3), dtype=np.uint8)
+    cv2.namedWindow("Draw")
+    global drawing, curr_x, curr_y
+    drawing = False
+
+    def mouse_callback(event, x, y, flags, param):
+        global drawing, curr_x, curr_y
+        if event == cv2.EVENT_LBUTTONDOWN:
+            drawing = True
+            curr_x, curr_y = x, y
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if drawing == True:
+                cv2.line(image, (curr_x, curr_y), (x, y), (255, 255, 255), 5)
+                curr_x = x
+                curr_y = y
+        elif event == cv2.EVENT_LBUTTONUP:
+            drawing = False
+            cv2.line(image, (curr_x, curr_y), (x, y), (255, 255, 255), 5)
+            curr_x = x
+            curr_y = y
+        return x, y
+
+    cv2.setMouseCallback('Draw', mouse_callback)
+    while (1):
+        cv2.imshow('Draw', 255 - image)
+        key = cv2.waitKey(10)
+        if key == ord(" "):
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            y, x = np.nonzero(image)
+            mn = min(np.min(y), np.min(x))
+            mx = max(np.max(y), np.max(x))
+            image = image[mn:mx, mn:mx]
+
+            image = cv2.resize(image, (28, 28), interpolation=cv2.INTER_AREA)
+            image = np.array(image, dtype=np.float32)[None, :, :, None]
+            pred_class = model.predict_classes(image)
+            classes = pickle.load(open("classes.p", "rb"))
+            #util.plotImage(image, classes[pred_class[0]])
+            print('Is it ' + classes[pred_class[0]] + '? ')
+            image = np.zeros((600, 600, 3), dtype=np.uint8)
+            curr_x = -1
+            curr_y = -1
+
+
+
+if __name__ == '__main__':
+    main()
--- src/experiment.py
+++ src/experiment.py
@@ -236,16 +236,16 @@ if __name__ == '__main__':
 
         elif i == 5:
             # MLP
-            for b in [1, 10, 50, 100]:
-                for e in [1, 10, 50, 100, 250]:
+            for b in [10, 50, 100]:
+                for e in [1, 10, 50]:
                     print('Running MLP model with batch size = ' + str(b) + ' and epochs = ' + str(e))
                     mlp_model = MlpModel(verbose=True, classes = len(classes))
                     loss = experiment3(mlp_model, x_train, y_train, x_dev, y_dev, 'mlp_b'+str(b)+'_e'+str(e), classes, b, e)
 
-        elif i == 6:
+            #elif i == 6:
             # CNN
-            for b in [50]: #[1, 10, 50, 100]:
-                for e in [1]: #[1, 10, 50, 100, 250]:
+            for b in [10, 50, 100]:
+                for e in [1, 10, 50]:
                     print('Running CNN model with batch size = ' + str(b) + ' and epochs = ' + str(e))
                     cnn_model = CnnModel(verbose=True, classes = len(classes))
                     loss = experiment3(cnn_model, x_train, y_train, x_dev, y_dev, 'cnn_b'+str(b)+'_e'+str(e), classes, b, e)
